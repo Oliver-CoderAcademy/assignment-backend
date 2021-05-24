900d51ecf955eacb8cd659d7c54c6386847a6657
Oliver-CoderAcademy <oliver.lavers@coderacademy.edu.au>
2021-05-24 22:46:31 +1000

    shonky...

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,6 @@ module.exports = {
     client: 'pg',
     connection: {
         host: process.env.DATABASE_URL || "127.0.0.1",
-        user: "zmgqjltregjdut",
-        password: "caecf6738399fbc9214770c9286915bc45d2067fef9559431b949ec113e5e60d",
-        database: "d50oi1156d38n6"
+        ssl: true
     }
 }
