78725877bd6143a74f886e80701946db7dda59d3
Oliver-CoderAcademy <oliver.lavers@coderacademy.edu.au>
2021-05-24 23:01:32 +1000

    shonky...

--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,7 @@ module.exports = {
         user: "zmgqjltregjdut",
         password: "caecf6738399fbc9214770c9286915bc45d2067fef9559431b949ec113e5e60d",
         database: "d50oi1156d38n6",
+        port: "5432",
+        ssl: true
     }
 }
